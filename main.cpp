2bca9f46333cfe799394efc690a16fd5c44f2314
RainLumosTaipei <zhou_zhengming@foxmail.com>
2025-03-09 15:42:38 +0800

    modify structure

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
-#include "Lexer.h"
-#include "SyntaxEntry.h"
-#include "SyntaxLl.h"
-#include "SyntaxLr.h"
+﻿#include "lex/Lexer.h"
+#include "syntax/SyntaxLl.h"
+#include "syntax/SyntaxLr.h"
+#include "syntax/SyntaxCheck.h"
 
 using namespace std;
 
-int main() {
 
-    token::lex();
-    //token::printTokens();
-
-    syntax::initSyntaxes();
+static void init()
+{
     syntax::ll::initFirst();
+    syntax::lr::initLr();
+    syntax::lr::saveTable();
     syntax::ll::printFirst();
     syntax::ll::printFollow();
+}
 
-    syntax::lr::initLr();
+static void test()
+{
+    syntax::lr::loadTable();
+    token::lex("open_set/03_arr_defn2.pas");
     syntax::lr::lrCheck();
+}
+
+int main() {
+
+    syntax::initSyntaxes();
+
+    //init();
+    test();
 
     return 0;
 }
